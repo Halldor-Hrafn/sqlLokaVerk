c990bbd3b8e5197bdb6f2ca79a744cb4d6ba2816
Halldor-sys <halldor.hrafn.reynisson@gmail.com>
2022-04-08 11:09:41 +0000

    new shit again

--- mangaCharacterInsertCode.sql
+++ mangaCharacterInsertCode.sql
@@ -17,22 +17,42 @@ VALUES ('Yuzuki', 'Tachibana', 17, 'Iwate prefecture, Japan', 'unknown'), ('Tama
 ('Ryo', 'Atsumi', 18, 'Chibe, Japan', 'unknown') --Primary characters you see in 'Taishou Otome Otogibanashi'
 
 INSERT INTO characters (firstName, lastName, age, birthplace, bloodType)
-VALUES ('Marin', 'Kitagawa', 15, 'Japan', 'unknown'), ('Wakana', 'Gojo', 15, 'Japan', 'unknown'),
+VALUES ('Marin', 'Kitagawa', 15, 'Japan', 'unknown'), ('Wakana', 'Gojo', 15, 'Japan', 'unknown'), --From 'My dress up darling'
 ('Sajuna', 'Inui', 17, 'Japan', 'unknown'), ('Shinju', 'Inui', 13, 'Japan', 'unknown')
 
 INSERT INTO characters (firstName, lastName, age, birthplace, bloodType)
-VALUES ('Micchon', 'Shikimori', 16, 'Japan', 'unknown'), ('Yuu', 'Izumi', 16, 'Japan', 'unknown'),
+VALUES ('Micchon', 'Shikimori', 16, 'Japan', 'unknown'), ('Yuu', 'Izumi', 16, 'Japan', 'unknown'), --From 'Shikimori's not just a cutie'
 ('Shuu', 'Inuzuka', 16, 'Japan', 'unknown'), ('Yui', 'Hachimitsu', 16, 'Japan', 'unknown'),
 ('Kyou', 'Nekozaki', 16, 'Japan', 'unknown')
 
 INSERT INTO characters (firstName, lastName, age, birthplace, bloodType)
-VALUES ('Kaname', 'Arisugawa', 15, 'Japan', 'unknown'), ('Aya', 'Arisugawa', 17, 'Japan', 'unknown'),
+VALUES ('Kaname', 'Arisugawa', 15, 'Japan', 'unknown'), ('Aya', 'Arisugawa', 17, 'Japan', 'unknown'), --From 'Tonikaku Kawaii'
 ('Tsukasa', 'Tsukuyomi', 16, 'Japan', 'O'), ('Chitose', 'Kaginoji', 14, 'Japan', 'unknown'),
 ('Kanoka', 'Yuzaki', 45, 'Japan', 'unknown')
 
 INSERT INTO characters (firstName, lastName, age, birthplace, bloodType)
-VALUES ('Nasa', 'Yuzaki', 17, 'Japan', 'A'), ('Enishi', 'Yuzaki', 45, 'Japan', 'unknown')
+VALUES ('Nasa', 'Yuzaki', 17, 'Japan', 'A'), ('Enishi', 'Yuzaki', 45, 'Japan', 'unknown') --from 'Tonikaku Kawaii'  
 
 INSERT INTO characters (firstName, lastName, age, birthplace, bloodType)
 VALUES ('Kuroto', 'Nakano', 30, 'Japan', 'unknown'), ('Senko', 'unknown', 800, 'unknown', 'unknown'),
-('Koenji', 'Yasuko', 20, 'Japan', 'unknown'), ('Shiro', 'unknown', 900, 'unknown', 'unknown')
+('Koenji', 'Yasuko', 20, 'Japan', 'unknown'), ('Shiro', 'unknown', 900, 'unknown', 'unknown') --From 'Senko-san'
+
+INSERT INTO characters (firstName, lastName, age, birthplace, bloodType)
+VALUES ('Mira', 'unknown', 16, 'Japan', 'unknown'), ('Soru', 'Hauru', 24, 'unknown', 'unknown'), --From 'Kenja no Deshi wo Nanoru Kenja'
+('Solomon', 'unknown', 16, 'unknown', 'unknown'), ('Luminaria', 'unknown', 22, 'unknown', 'unknown'), --Holy shit a lot of this series is unknown
+('Mariana', 'unknown', 50, 'unknown', 'unknown'), ('Tact', 'unknown', 11, 'unknown', 'unknown')
+
+INSERT INTO characters (firstName, lastName, age, birthplace, bloodType)
+VALUES ('unknown', 'Takagi', 14, 'Japan', 'O'), ('unknown', 'Nishikata', 14, 'Japan', 'O'), --From 'Karakai Jouzu no Takagi-san'
+('Mina', 'Hibino', 14, 'Japan', 'unknown'), ('Yukari', 'Tenkawa', 14, 'Japan', 'unknown'),
+('Sanae', 'Tsukimoto', 14, 'Japan', 'unknown')
+
+INSERT INTO characters (firstName, lastName, age, birthplace, bloodType)
+VALUES ('Takagi', 'Nishikata', 26, 'Japan', 'O'), ('unknown', 'Nishikata', 26, 'Japan', 'O'), --From 'Karakai Jouzu no (Moto) takagi-san'
+('Chi', 'Nishikata', 4, 'Japan', 'O')
+
+INSERT INTO characters (firstName, lastName, age, birthplace, bloodType)
+VALUES ('Hajime', 'Nagumo', 17, 'Japan', 'unknown'), ('Kaori', 'Shirasaki', 17, 'Japan', 'unknown'), --From Arifureta
+('Shizuki', 'Yaegashi', 17, 'Japan', 'unknown'), ('Yue', 'unknown', 323, 'unknown', 'unknown'),
+('Tio', 'Klarus', 563, 'unknown', 'unknown'), ('Shea', 'Haulia', 16, 'unknown', 'unknown'),
+('Myu', 'unknown', 4, 'unknown', 'unknown')
--- mangasInsertCode.sql
+++ mangasInsertCode.sql
@@ -5,3 +5,11 @@ VALUES ('K-ON!', 4, 56.5, 'completed'), ('Maou-sama to Kekkon shitai', 3, 18, 'c
 INSERT INTO mangas (title, volumes, chapters, publication)
 VALUES ('Sono Bisque Doll wa Koi wo Suru', 8, 74, 'ongoing'), ("Shikimori's Not Just a Cutie", 12, 142, 'ongoing'),
 ('Tonikaku Kawaii', 9, 185, 'ongoing'), ('Sewayaki Kitsune no Senko-san', 11, 82.5, 'ongoing')
+
+INSERT INTO mangas (title, volumes, chapters, publication)
+VALUES ('Kenja no Deshi wo Nanoru Kenja', 8, 52.2, 'ongoing'), ('Karakai Jouzu no Takagi-san', 17, 158, 'ongoing'),
+('Karakai Jouzu no (Moto) Takagi-san', 15, 224, 'ongoing'), ('Arifureta Shokugyou de Sekai Saikyou', 9, 57, 'ongoing')
+
+INSERT INTO mangas (title, volumes, chapters, publication)
+VALUES ('Suki na Ko ga Megane wo Wasureta', 9, 86, 'ongoing'), ('Shachiku-san wa Youjo Yuurei ni Iyasaretai', 7, 34, 'ongoing'),
+('Oku-sama wa Nizzuma-chan', 4, 24, 'ongoing'), ('Hanging Out with a Gamer Girl', 6, 108, 'ongoing')
